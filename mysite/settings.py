8ef7352d6382c5d73930fabd630e96ac06d5a07a
Viki2608 <vignesh26.manoharan@gmail.com>
2022-01-16 16:51:53 +0530

    first commit1

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0p+&pdi=-phh4fyt8$xtts955=nyvyethymsxyt-qn@2)thkc^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['vigneshblog.herokuapp.com','127.0.0.1']
 
@@ -83,6 +83,7 @@ DATABASES = {
         'NAME': 'blog',
         'USER' : 'postgres',
         'PASSWORD':'admin',
+        'HOST':'localhost',
     }
 }
 
